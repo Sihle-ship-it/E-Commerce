3dd650731cf7647e93e04d732a7f10689cf95e0b
Sihle Mthembu <innocentsihle29@gmail.com>
2023-02-17 17:08:52 +0200

    bugfix on basket controller

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -6,7 +6,7 @@ using System.Net;
 namespace Basket.API.Controllers
 {
     [ApiController]
-    [Route("api/vi/[controller]")]
+    [Route("api/v1/[controller]")]
     public class BasketController: ControllerBase
     {
         private readonly IBasketRepository _repository;
