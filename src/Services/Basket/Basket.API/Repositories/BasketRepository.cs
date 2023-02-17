e64bda07336dda890506c26fe6ac05d60a87f598
Sihle Mthembu <innocentsihle29@gmail.com>
2023-02-17 15:10:01 +0200

    Added a repository pattern for basket service

--- src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -1,6 +1,38 @@
-﻿namespace Basket.API.Repositories
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
+using Newtonsoft.Json;
+
+namespace Basket.API.Repositories
 {
-    public class BasketRepository
+    public class BasketRepository : IBasketRepository
     {
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache ?? throw new ArgumentNullException(nameof(RedisCache));
+        }
+        
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _redisCache.GetStringAsync(userName);
+            if (String.IsNullOrEmpty(basket)) return null;
+            
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await _redisCache.SetStringAsync(basket.UserName,JsonConvert.SerializeObject(basket));
+
+            return await GetBasket(basket.UserName);
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+            await _redisCache.RemoveAsync(userName);
+        }
     }
 }
